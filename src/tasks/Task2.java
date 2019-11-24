d1a57c4db96089c80df3080dfb855a0df292289f
Nikita.Voronov <Nikita.Voronov@outlook.com>
2019-11-24 14:08:05 +0300

    Task2 solved

--- src/tasks/Task2.java
+++ src/tasks/Task2.java
@@ -4,11 +4,10 @@ import common.Person;
 import common.Task;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 Задача 2
@@ -22,7 +21,10 @@ public class Task2 implements Task {
   private static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                       Collection<Person> persons2,
                                                       int limit) {
-    return new ArrayList<>();
+    return Stream.concat(persons1.stream(),persons2.stream())
+            .sorted(Comparator.comparing(Person::getCreatedAt))
+            .limit(limit)
+            .collect(Collectors.toList());
   }
 
   @Override
